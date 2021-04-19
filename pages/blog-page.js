657a94351d7f5559e77815bea4a91f39947efb37
yasumoto <yasumoto@aspcs.co.jp>
2021-04-19 12:50:35 +0900

    getStaticProps

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,8 +1,22 @@
 import Layout from "../components/Layout";
+import Post from '../components/Post'
+import { getAllPostsData } from "../lib/posts";
 
-
-export const Blog = () => {
-  return <Layout title="Blog">Blog page</Layout>;
+export const Blog = ({posts}) => {
+  return (
+    <Layout title="Blog">
+      <ul className="m-10">
+        {posts && posts.map((post) => <Post key={post.id} post={post} />)}
+      </ul>
+    </Layout>
+  );
 };
 
 export default Blog;
+
+export async function getStaticProps() {
+  const posts = await getAllPostsData();
+  return {
+    props: { posts },
+  };
+}
